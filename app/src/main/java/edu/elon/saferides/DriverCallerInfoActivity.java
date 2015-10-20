e8103af18161cfd25ad36f26f971bb206289333b
Maddie <Owner@maddies-mbp-2.elon.edu>
2015-10-20 16:50:17 -0400

    safe rides app1

---
+++ app/src/main/java/edu/elon/saferides/DriverCallerInfoActivity.java
@@ -0,0 +1,17 @@
+package edu.elon.saferides;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class DriverCallerInfoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_caller_info);
+    }
+
+
+}
