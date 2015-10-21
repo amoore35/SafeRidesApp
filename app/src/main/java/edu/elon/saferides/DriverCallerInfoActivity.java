6214769000ee207fca29f69dcba406885fde6417
Lawrence Mullen <lmullen3@elon.edu>
2015-10-21 14:07:18 -0400

    added map activity and xml file

--- app/src/main/java/edu/elon/saferides/DriverCallerInfoActivity.java
+++ app/src/main/java/edu/elon/saferides/DriverCallerInfoActivity.java
@@ -1,17 +1,14 @@
 package edu.elon.saferides;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class DriverCallerInfoActivity extends Activity {
+public class DriverCallerInfoActivity extends Fragment {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_driver_caller_info);
-    }
+
 
 
 }
--- app/src/main/java/edu/elon/saferides/RiderInfoCheckActivity.java
+++ app/src/main/java/edu/elon/saferides/RiderInfoCheckActivity.java
@@ -1,38 +1,32 @@
 package edu.elon.saferides;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class RiderInfoCheckActivity extends Activity {
+
+    private TextView phoneView;
+    private TextView nameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rider_info_check);
+
+        phoneView = (TextView) findViewById(R.id.phone_view);
+        nameView = (TextView) findViewById(R.id.name_view);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("nameInput");
+        String number = intent.getStringExtra("phoneNumberInput");
+        nameView.setText(name);
+        phoneView.setText(number);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_rider_info_check, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
