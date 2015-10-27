c23f27b22d17255720308bcee68315ce5bef50c9
catharine <catharine.haglund@gmail.com>
2015-10-27 19:51:49 -0400

    driver accpets riders request

---
+++ app/src/main/java/edu/elon/saferides/AcceptedCallerActivity.java
@@ -0,0 +1,17 @@
+package edu.elon.saferides;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+
+public class AcceptedCallerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accepted_caller);
+    }
+
+
+}
--- app/src/main/java/edu/elon/saferides/DriverCallerInfoActivity.java
+++ app/src/main/java/edu/elon/saferides/DriverCallerInfoActivity.java
@@ -2,9 +2,11 @@ package edu.elon.saferides;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class DriverCallerInfoActivity extends Activity {
 
@@ -12,6 +14,10 @@ public class DriverCallerInfoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_caller_info);
+    }
+    public void onClickAccept (View view){
+        Intent intent = new Intent(this, AcceptedCallerActivity.class);
+        startActivity(intent);
     }
 
 }
