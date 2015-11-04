bda978c12b3ab06e1cd4c11cb6e8ebd0fb350f59
catharine <catharine.haglund@gmail.com>
2015-11-03 20:58:38 -0500

    Changed colors to make it more pretty, let the rider know when their information has been sent

--- app/src/main/java/edu/elon/saferides/RiderInfoCheckActivity.java
+++ app/src/main/java/edu/elon/saferides/RiderInfoCheckActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 
@@ -27,6 +28,12 @@ public class RiderInfoCheckActivity extends Activity {
         phoneView.setText(number);
     }
 
+    public void onClickNext(View view){
+        Intent intent = new Intent(this, RiderRequestSentActivity.class);
+
+        startActivity(intent);
+
+    }
 
 
 }
---
+++ app/src/main/java/edu/elon/saferides/RiderRequestSentActivity.java
@@ -0,0 +1,14 @@
+package edu.elon.saferides;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class RiderRequestSentActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rider_request_sent);
+    }
+
+}
