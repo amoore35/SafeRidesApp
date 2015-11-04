b2269f44305a677e9399af4b84a46734eb80b5ff
Lawrence Mullen <lmullen3@elon.edu>
2015-11-04 13:08:20 -0500

    maps

--- app/src/main/java/edu/elon/saferides/DriverCallerInfoActivity.java
+++ app/src/main/java/edu/elon/saferides/DriverCallerInfoActivity.java
@@ -1,19 +1,36 @@
 package edu.elon.saferides;
-
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
-public class DriverCallerInfoActivity extends Activity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class DriverCallerInfoActivity extends Activity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_caller_info);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+    @Override
+    public void onMapReady(GoogleMap map){
+        LatLng here = new LatLng(-33.867,151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(here, 13));
+
+        map.addMarker(new MarkerOptions().title("here").position(here));
+
+
     }
     public void onClickAccept (View view){
         Intent intent = new Intent(this, AcceptedCallerActivity.class);
